b132bfaee97209ed4cd52128740558421e620f39
sayaka <salyaka623@gamil.com>
2021-02-12 23:35:17 +0900

    新規投稿機能と一覧表示機能を実装

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,14 +1,24 @@
 class ScoresController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @scores = current_user.score.all.order("created_at DESC")
   end
 
   def show
   end
 
   def new
+    @score = Score.new
   end
 
   def create
+    @score = current_user.score.create(score_params)
+    if @score.save
+      redirect_to :scores
+    else
+      render action: :new
+    end
   end
 
   def edit
@@ -18,5 +28,11 @@ class ScoresController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def score_params
+    params.require(:score).permit(:title, :grade, :start_date, :completion_date, :icon, :status)
   end
 end
