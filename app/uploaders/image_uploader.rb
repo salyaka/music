503bab9f4e7780ac42018d93aafd8cb922edfb47
sayaka <salyaka623@gamil.com>
2021-02-14 15:13:26 +0900

    ファイル名をランダムに変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -50,4 +50,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   process resize_to_limit: [140, 210]
+
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
 end
