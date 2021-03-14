e6c1db0ec8fb430351534e095ef5f62bcc0417fb
sayaka <salyaka623@gamil.com>
2021-03-14 16:31:55 +0900

    ゲストログイン機能の設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#new_guest"
+  end
   root "static_pages#home"
   resources :scores
   resources :logs, only: [:create, :destroy, :index]
