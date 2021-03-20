bdac1a644d227e8fb53874887aeba2ac96ef8e2c
sayaka <salyaka623@gamil.com>
2021-03-20 16:24:51 +0900

    バリデーションのテストを追加

--- spec/models/log_spec.rb
+++ spec/models/log_spec.rb
@@ -12,11 +12,27 @@ RSpec.describe Log, type: :model do
       end
     end
 
+    context "contentが200文字以上のとき" do
+      let(:log) { build(:log, content: "あ" * 201) }
+      it "エラーが発生する" do
+        expect(subject).to eq false
+        expect(log.errors[:content]).to include "は200文字以内で入力してください"
+      end
+    end
+
     context "start_time" do
       let(:log) { build(:log, start_time: "") }
       it "エラーが発生する" do
         expect(subject).to eq false
         expect(log.errors[:start_time]).to include "を入力してください"
+      end
+    end
+
+    context "start_time" do
+      let(:log) { build(:log, start_time: Date.today + 1) }
+      it "エラーが発生する" do
+        expect(subject).to eq false
+        expect(log.errors[:start_time]).to include "未来の日付は登録できません"
       end
     end
 
