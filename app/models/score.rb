73d75f0d737c06dd0aef48424aca19f595184408
sayaka <salyaka623@gamil.com>
2021-04-11 21:54:37 +0900

    楽譜の完成日に開始日以前の日付を登録できないように設定

--- app/models/score.rb
+++ app/models/score.rb
@@ -3,6 +3,7 @@ class Score < ApplicationRecord
   has_many :logs, dependent: :destroy
   validates :title, presence: true, length: { in: 1..50 }
   validates :point, allow_blank: true, length: { maximum: 100 }
+  validate :completion_date_before
   mount_uploader :icon, ImageUploader
 
   enum status: {
@@ -14,6 +15,12 @@ class Score < ApplicationRecord
 
   def feed_log(score_id)
     Log.where("score_id = ?", score_id)
+  end
+
+  def completion_date_before
+    if start_date.present? && completion_date < start_date
+      errors.add(:completion_date, "は開始日以前の日付で登録することはできません。")
+    end
   end
 
 end
